7e490c92302f518a8cbf8204bbd9f1a96fde5a08
UsherBaby <io.2016@outlook.com>
2017-04-24 21:07:13 +0800

    refactor

---
+++ scheduler-transformer/transformer/src/main/java/com/thepacific/transformer/MaybeTransformerUtil.java
@@ -0,0 +1,93 @@
+package com.thepacific.transformer;
+
+import com.trello.rxlifecycle2.RxLifecycle;
+
+import java.util.concurrent.Executor;
+
+import javax.annotation.Nonnull;
+
+import io.reactivex.Maybe;
+import io.reactivex.MaybeSource;
+import io.reactivex.MaybeTransformer;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class MaybeTransformerUtil {
+
+    public static <T, R> MaybeTransformer<T, T> io(@Nonnull final Observable<R> lifecycle,
+                                                   @Nonnull final R event) {
+        return new MaybeTransformer<T, T>() {
+            @Override
+            public MaybeSource<T> apply(Maybe<T> maybe) {
+                return maybe.subscribeOn(Schedulers.io())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> MaybeTransformer<T, T> computation(@Nonnull final Observable<R> lifecycle,
+                                                            @Nonnull final R event) {
+        return new MaybeTransformer<T, T>() {
+            @Override
+            public MaybeSource<T> apply(Maybe<T> maybe) {
+                return maybe.subscribeOn(Schedulers.computation())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> MaybeTransformer<T, T> trampoline(@Nonnull final Observable<R> lifecycle,
+                                                           @Nonnull final R event) {
+        return new MaybeTransformer<T, T>() {
+            @Override
+            public MaybeSource<T> apply(Maybe<T> maybe) {
+                return maybe.subscribeOn(Schedulers.trampoline())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> MaybeTransformer<T, T> newThread(@Nonnull final Observable<R> lifecycle,
+                                                          @Nonnull final R event) {
+        return new MaybeTransformer<T, T>() {
+            @Override
+            public MaybeSource<T> apply(Maybe<T> maybe) {
+                return maybe.subscribeOn(Schedulers.newThread())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> MaybeTransformer<T, T> single(@Nonnull final Observable<R> lifecycle,
+                                                       @Nonnull final R event) {
+        return new MaybeTransformer<T, T>() {
+            @Override
+            public MaybeSource<T> apply(Maybe<T> maybe) {
+                return maybe.subscribeOn(Schedulers.single())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> MaybeTransformer<T, T> from(@Nonnull final Observable<R> lifecycle,
+                                                     @Nonnull final R event,
+                                                     @Nonnull final Executor executor) {
+        return new MaybeTransformer<T, T>() {
+            @Override
+            public MaybeSource<T> apply(Maybe<T> maybe) {
+                return maybe.subscribeOn(Schedulers.from(executor))
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    private MaybeTransformerUtil() {
+    }
+}
