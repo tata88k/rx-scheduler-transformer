30676271b427326b0f2845b3b0ec55ab5922f9ac
UsherBaby <io.2016@outlook.com>
2017-04-24 20:43:43 +0800

    init project

---
+++ scheduler-transformer/transformer/src/main/java/com/thepacific/demo/FlowableTransformerUtil.java
@@ -0,0 +1,93 @@
+package com.thepacific.demo;
+
+import com.trello.rxlifecycle2.RxLifecycle;
+
+import org.reactivestreams.Publisher;
+
+import java.util.concurrent.Executor;
+
+import javax.annotation.Nonnull;
+
+import io.reactivex.Flowable;
+import io.reactivex.FlowableTransformer;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class FlowableTransformerUtil {
+    public static <T, R> FlowableTransformer<T, T> io(@Nonnull final Observable<R> lifecycle,
+                                                      @Nonnull final R event) {
+        return new FlowableTransformer<T, T>() {
+            @Override
+            public Publisher<T> apply(Flowable<T> flowable) {
+                return flowable.subscribeOn(Schedulers.io())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> FlowableTransformer<T, T> computation(@Nonnull final Observable<R> lifecycle,
+                                                               @Nonnull final R event) {
+        return new FlowableTransformer<T, T>() {
+            @Override
+            public Publisher<T> apply(Flowable<T> flowable) {
+                return flowable.subscribeOn(Schedulers.computation())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> FlowableTransformer<T, T> trampoline(@Nonnull final Observable<R> lifecycle,
+                                                              @Nonnull final R event) {
+        return new FlowableTransformer<T, T>() {
+            @Override
+            public Publisher<T> apply(Flowable<T> flowable) {
+                return flowable.subscribeOn(Schedulers.trampoline())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> FlowableTransformer<T, T> newThread(@Nonnull final Observable<R> lifecycle,
+                                                             @Nonnull final R event) {
+        return new FlowableTransformer<T, T>() {
+            @Override
+            public Publisher<T> apply(Flowable<T> flowable) {
+                return flowable.subscribeOn(Schedulers.newThread())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> FlowableTransformer<T, T> single(@Nonnull final Observable<R> lifecycle,
+                                                          @Nonnull final R event) {
+        return new FlowableTransformer<T, T>() {
+            @Override
+            public Publisher<T> apply(Flowable<T> flowable) {
+                return flowable.subscribeOn(Schedulers.io())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> FlowableTransformer<T, T> from(@Nonnull final Observable<R> lifecycle,
+                                                        @Nonnull final R event,
+                                                        @Nonnull final Executor executor) {
+        return new FlowableTransformer<T, T>() {
+            @Override
+            public Publisher<T> apply(Flowable<T> flowable) {
+                return flowable.subscribeOn(Schedulers.from(executor))
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    private FlowableTransformerUtil() {
+    }
+}
---
+++ scheduler-transformer/transformer/src/main/java/com/thepacific/demo/SingleTransformerUtil.java
@@ -0,0 +1,92 @@
+package com.thepacific.demo;
+
+import com.trello.rxlifecycle2.RxLifecycle;
+
+import java.util.concurrent.Executor;
+
+import javax.annotation.Nonnull;
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.SingleSource;
+import io.reactivex.SingleTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class SingleTransformerUtil {
+    public static <T, R> SingleTransformer<T, T> io(@Nonnull final Observable<R> lifecycle,
+                                                    @Nonnull final R event) {
+        return new SingleTransformer<T, T>() {
+            @Override
+            public SingleSource<T> apply(Single<T> single) {
+                return single.subscribeOn(Schedulers.io())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> SingleTransformer<T, T> computation(@Nonnull final Observable<R> lifecycle,
+                                                             @Nonnull final R event) {
+        return new SingleTransformer<T, T>() {
+            @Override
+            public SingleSource<T> apply(Single<T> single) {
+                return single.subscribeOn(Schedulers.computation())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> SingleTransformer<T, T> trampoline(@Nonnull final Observable<R> lifecycle,
+                                                            @Nonnull final R event) {
+        return new SingleTransformer<T, T>() {
+            @Override
+            public SingleSource<T> apply(Single<T> single) {
+                return single.subscribeOn(Schedulers.trampoline())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> SingleTransformer<T, T> newThread(@Nonnull final Observable<R> lifecycle,
+                                                           @Nonnull final R event) {
+        return new SingleTransformer<T, T>() {
+            @Override
+            public SingleSource<T> apply(Single<T> single) {
+                return single.subscribeOn(Schedulers.newThread())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> SingleTransformer<T, T> single(@Nonnull final Observable<R> lifecycle,
+                                                        @Nonnull final R event) {
+        return new SingleTransformer<T, T>() {
+            @Override
+            public SingleSource<T> apply(Single<T> single) {
+                return single.subscribeOn(Schedulers.single())
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static <T, R> SingleTransformer<T, T> from(@Nonnull final Observable<R> lifecycle,
+                                                      @Nonnull final R event,
+                                                      @Nonnull final Executor executor) {
+        return new SingleTransformer<T, T>() {
+            @Override
+            public SingleSource<T> apply(Single<T> single) {
+                return single.subscribeOn(Schedulers.from(executor))
+                        .compose(RxLifecycle.<T, R>bindUntilEvent(lifecycle, event))
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    private SingleTransformerUtil() {
+    }
+}
